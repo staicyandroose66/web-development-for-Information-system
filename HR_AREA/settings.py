64dd6fa280ee16fde46be6c402a4471f052f8f16
staicyandroose66 <71206677+staicyandroose66@users.noreply.github.com>
2023-04-04 22:43:01 +0530

    Update settings.py

--- HR_AREA/settings.py
+++ HR_AREA/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
